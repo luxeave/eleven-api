3451f8aac150848d5124fc5d07527355cba254ea
stephen antoni <luxeaves@gmail.com>
2024-11-12 16:20:22 +0700

    adjusted expected payload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Directory to save MP3 files
 const AUDIO_DIR = '/var/www/html/audio/';
